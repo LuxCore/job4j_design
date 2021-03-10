e88876dd66be576e3f7ec7e4b86a36f97b10d066
LuxCore <denis.kitrish@outlook.com>
2021-03-10 08:46:01 +0200

    0. Что такое обобщенные типы (generics).

--- src/main/java/ru/job4j/generics/Generics.java
+++ src/main/java/ru/job4j/generics/Generics.java
@@ -1,11 +1,40 @@
 package ru.job4j.generics;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Класс с утилитами для проверки обобщений.
  */
 public class Generics {
+	/**
+	 * Проверка обощений.
+	 * @param args Аргументы командной строки.
+	 */
+	public static void main(final String[] args) {
+		Generics gen = new Generics();
+		List<Animal> first = new ArrayList<>();
+		List<Predator> second = new ArrayList<>();
+		List<Tiger> third = new ArrayList<>();
+		first.add(new Animal());
+		second.add(new Predator());
+		third.add(new Tiger());
+
+		gen.printObject(first);
+		gen.printObject(second);
+		gen.printObject(third);
+		System.out.println();
+
+//		gen.printBoundedWildCard(first);
+		gen.printBoundedWildCard(second);
+		gen.printBoundedWildCard(third);
+		System.out.println();
+
+		gen.printLowerBoundedWildCard(first);
+		gen.printLowerBoundedWildCard(second);
+//		gen.printLowerBoundedWildCard(third);
+	}
+
 	/**
 	 * Метод печати списка с любым содержимым.
 	 * @param list Список с любым содержимым.
