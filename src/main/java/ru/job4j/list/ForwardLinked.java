6acbd0be572524f9cbd46bf83aad315d438deb01
LuxCore <denis.kitrish@outlook.com>
2021-06-15 08:46:40 +0300

    3. Удалить head в односвязном списке. [#51424]

---
+++ src/main/java/ru/job4j/list/ForwardLinked.java
@@ -0,0 +1,125 @@
+package ru.job4j.list;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
+/**
+ * Однонаправленный связный список.
+ * @param <T> тип значений, которые может содержать связный список.
+ */
+public class ForwardLinked<T> implements Iterable<T> {
+	/**
+	 * Первый элемент списка.
+	 */
+	private Node<T> head;
+	/**
+	 * Количество изменений в списке.
+	 */
+	private int modificationCount;
+
+	/**
+	 * Узел списка, содержащий значение.
+	 * @param <V> Тип значения списка.
+	 */
+	private static class Node<V> {
+		/**
+		 * Значение узла.
+		 */
+		private V value;
+		/**
+		 * Указатель на следующий узел.
+		 */
+		private Node<V> next;
+
+		/**
+		 * Конструктор узла.
+		 * @param value значение узла.
+		 * @param next указатель на следующий узел.
+		 */
+		Node(final V value, final Node<V> next) {
+			this.value = value;
+			this.next = next;
+		}
+	}
+
+	/**
+	 * Добавляет новое значение в конец списка.
+	 * @param value новое значение, которое необходимо добавить в список.
+	 */
+	public void add(final T value) {
+		Node<T> node = new Node<>(value, null);
+		if (head == null) {
+			head = node;
+			return;
+		}
+		Node<T> tail = head;
+		while (tail.next != null) {
+			tail = tail.next;
+		}
+		tail.next = node;
+		modificationCount++;
+	}
+
+	/**
+	 * Удаление головного элемента списка.
+	 * @return значение головного элемента.
+	 */
+	public T deleteFirst() {
+		Objects.requireNonNull(head, "ForwardLinked is empty");
+		Node<T> temp = head;
+		head = temp.next;
+		temp.next = null;
+		modificationCount++;
+		return temp.value;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public Iterator<T> iterator() {
+		return new Itr();
+	}
+
+	/**
+	 * Итератор по связному списку.
+	 */
+	private class Itr implements Iterator<T> {
+		/**
+		 * Указатель на следующий узел.
+		 */
+		private Node<T> pointer = head;
+		/**
+		 * Ожидаемое количество изменений в списке.
+		 */
+		private int expectedModificationCount = modificationCount;
+
+		/**
+		 * {@inheritDoc}
+		 */
+		@Override
+		public boolean hasNext() {
+			return pointer != null;
+		}
+
+		/**
+		 * {@inheritDoc}
+		 * @throws ConcurrentModificationException в случае, если список был
+		 * изменён во время итерации.
+		 */
+		@Override
+		public T next() {
+			if (expectedModificationCount != modificationCount) {
+				throw new ConcurrentModificationException();
+			}
+			if (!hasNext()) {
+				throw new NoSuchElementException();
+			}
+			T result = pointer.value;
+			pointer = pointer.next;
+			return result;
+		}
+	}
+}
---
+++ src/test/java/ru/job4j/list/ForwardLinkedTest.java
@@ -0,0 +1,90 @@
+package ru.job4j.list;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class ForwardLinkedTest {
+
+	@Test
+	void add1() {
+		ForwardLinked<String> fl = new ForwardLinked<>();
+		fl.add("One");
+		Iterator<String> it = fl.iterator();
+		assertAll(
+				() -> assertEquals("One", it.next()),
+				() -> assertThrows(NoSuchElementException.class, it::next)
+		);
+	}
+
+	@Test
+	void add2() {
+		ForwardLinked<String> fl = new ForwardLinked<>();
+		fl.add("One");
+		fl.add("Two");
+		fl.add("Three");
+		fl.add("6669");
+		Iterator<String> it = fl.iterator();
+		assertAll(
+				() -> assertEquals("One", it.next()),
+				() -> assertEquals("Two", it.next()),
+				() -> assertEquals("Three", it.next()),
+				() -> assertEquals("6669", it.next()),
+				() -> assertThrows(NoSuchElementException.class, it::next)
+		);
+	}
+
+	@Test
+	void deleteFirstWhenEmpty() {
+		ForwardLinked<Integer> fl = new ForwardLinked<>();
+		assertThrows(NullPointerException.class, fl::deleteFirst);
+	}
+
+	@Test
+	void deleteFirst() {
+		ForwardLinked<Integer> fl = new ForwardLinked<>();
+		fl.add(1);
+		fl.add(2);
+		fl.add(3);
+		fl.add(6669);
+		assertAll(
+				() -> assertEquals(1, fl.deleteFirst()),
+				() -> assertEquals(2, fl.deleteFirst()),
+				() -> assertEquals(3, fl.deleteFirst()),
+				() -> assertEquals(6669, fl.deleteFirst()),
+				() -> assertThrows(NullPointerException.class, fl::deleteFirst),
+				() -> assertThrows(NoSuchElementException.class, fl.iterator()::next)
+		);
+	}
+
+	@Test
+	void addDuringIteration() {
+		ForwardLinked<Integer> fl = new ForwardLinked<>();
+		fl.add(1);
+		Iterator<Integer> it = fl.iterator();
+		fl.add(6669);
+		assertThrows(ConcurrentModificationException.class, it::next);
+	}
+
+	@Test
+	void deleteDuringIteration() {
+		ForwardLinked<Integer> fl = new ForwardLinked<>();
+		fl.add(1);
+		fl.add(6669);
+		Iterator<Integer> it = fl.iterator();
+		fl.deleteFirst();
+		assertThrows(ConcurrentModificationException.class, it::next);
+	}
+
+	@Test
+	void iterateWhenEmpty() {
+		ForwardLinked<Integer> fl = new ForwardLinked<>();
+		assertThrows(NoSuchElementException.class, fl.iterator()::next);
+	}
+}
